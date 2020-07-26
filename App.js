64a4a93eff057c3bc0f8184c259f34a16e0ff0ff
NoelRomer602 <noelromerostudent@gmail.com>
2020-07-26 18:15:34 -0500

    added the rep counter

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import Clock from '../AIfitness/components/clock';
+import Clock from './components/clock';
+import RepCounter from './components/RepCounter';
 export default function App() {
   let isRestarted = false;
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+     <RepCounter/>
       <Clock data={isRestarted}/>
       <StatusBar style="auto" />
       <Button title="Restart" onPress={() => {isRestarted = true;}}/>
     </View>
   );
-}
-
+} 
+   
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -22,3 +23,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+ 
---
+++ components/RepCounter.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+class RepCounter extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            count:0
+        };
+    }
+
+    onPress = () => {
+        this.setState({
+            count: this.state.count +1
+        });
+    };
+
+    render(){
+        const {count} = this.state;
+        return(
+            <View >
+                <View style={styles.countContainer}>
+        <Text>Rep#: {count}</Text>
+                </View>
+                <TouchableOpacity style={styles.button}
+          onPress={this.onPress}>click here</TouchableOpacity>
+            </View>
+        )
+
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      paddingHorizontal: 10
+    },
+    button: {
+      alignItems: "center",
+      backgroundColor: "#DDDDDD",
+      padding: 10
+    },
+    countContainer: {
+      alignItems: "center",
+      padding: 10
+    }
+  });
+  export default RepCounter;
